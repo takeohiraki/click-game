509d1a99af01f0f158ba17b0db14366c9b7434e1
takeohiraki <takeohiraki@gmail.com>
2019-08-29 01:15:50 -0700

    heroku postbuild

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Body from "./components/Body";
 
-function App() {
-  return <Body />;
+// function App() {
+  /* return <Body />; */
+// }
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    {/* <div id="app"> */}
+  
+        <Switch>
+          <Route path="/" exact component={Body} />
+        </Switch>
+    {/* </div> */}
+  </BrowserRouter>
+  )  
 }
+
 
 export default App;
